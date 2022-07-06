7c4215e686fdb9c6f85e398272d189ee9ec73306
Mohammad Hefny <mohammad.hefny@gmail.com>
2022-07-06 08:15:22 +0200

    upgrade: usb hoho driver

--- ClientLib/src/main/java/com/hoho/android/usbserial/driver/UsbId.java
+++ ClientLib/src/main/java/com/hoho/android/usbserial/driver/UsbId.java
@@ -1,22 +1,9 @@
-/* Copyright 2012 Google Inc.
+/* Copyright 2011-2013 Google Inc.
+ * Copyright 2013 mike wakerly <opensource@hoho.com>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
- *
- * Project home page: http://code.google.com/p/usb-serial-for-android/
+ * Project home page: https://github.com/mik3y/usb-serial-for-android
  */
+
 package com.hoho.android.usbserial.driver;
 
 /**
@@ -34,15 +21,7 @@ public final class UsbId {
     public static final int FTDI_FT2232H = 0x6010;
     public static final int FTDI_FT4232H = 0x6011;
     public static final int FTDI_FT232H = 0x6014;
-    public static final int FTDI_FT231X = 0x6015;
-    public static final int DEVICE_PIXHAWK_2_CUBE = 0x0016;
-
-    public static final int VENDOR_PX4    = 0x26AC;
-    public static final int DEVICE_PX4FMU = 0x0011;
-
-    public static final int VENDOR_ARDUINO2 = 0x26ac;
-    public static final int PIXHAWK = 0x0011;
-
+    public static final int FTDI_FT231X = 0x6015; // same ID for FT230X, FT231X, FT234XD
 
     public static final int VENDOR_ATMEL = 0x03EB;
     public static final int ATMEL_LUFA_CDC_DEMO_APP = 0x2044;
@@ -64,15 +43,35 @@ public final class UsbId {
 
     public static final int VENDOR_LEAFLABS = 0x1eaf;
     public static final int LEAFLABS_MAPLE = 0x0004;
-    
-    public static final int VENDOR_SILAB = 0x10c4;
-    public static final int SILABS_CP2102 = 0xea60;
+
+    public static final int VENDOR_SILABS = 0x10c4;
+    public static final int SILABS_CP2102 = 0xea60; // same ID for CP2101, CP2103, CP2104, CP2109
     public static final int SILABS_CP2105 = 0xea70;
     public static final int SILABS_CP2108 = 0xea71;
     public static final int SILABS_CP2110 = 0xea80;
 
     public static final int VENDOR_PROLIFIC = 0x067b;
-    public static final int PROLIFIC_PL2303 = 0x2303;
+    public static final int PROLIFIC_PL2303 = 0x2303;   // device type 01, T, HX
+    public static final int PROLIFIC_PL2303GC = 0x23a3; // device type HXN
+    public static final int PROLIFIC_PL2303GB = 0x23b3; // "
+    public static final int PROLIFIC_PL2303GT = 0x23c3; // "
+    public static final int PROLIFIC_PL2303GL = 0x23d3; // "
+    public static final int PROLIFIC_PL2303GE = 0x23e3; // "
+    public static final int PROLIFIC_PL2303GS = 0x23f3; // "
+
+    public static final int VENDOR_QINHENG = 0x1a86;
+    public static final int QINHENG_CH340 = 0x7523;
+    public static final int QINHENG_CH341A = 0x5523;
+
+    // at www.linux-usb.org/usb.ids listed for NXP/LPC1768, but all processors supported by ARM mbed DAPLink firmware report these ids
+    public static final int VENDOR_ARM = 0x0d28;
+    public static final int ARM_MBED = 0x0204;
+
+    public static final int VENDOR_ST = 0x0483;
+    public static final int ST_CDC = 0x5740;
+
+    public static final int VENDOR_RASPBERRY_PI = 0x2e8a;
+    public static final int RASPBERRY_PI_PICO_MICROPYTHON = 0x0005;
 
     public static final int VENDOR_UBLOX = 0x1546;
     public static final int DEVICE_UBLOX_5 = 0x01a5;
@@ -98,41 +97,37 @@ public final class UsbId {
     public static final int DEVICE_RADIOLINK_MINI = 0x0016;
 
 
-    public static final int VENDOR_ARDUPILOT_FUTURE  = 0x16D0;
-    public static final int DEVICE_ARDUPILOT_FUTURE  = 0x0E65;
+    public static final int VENDOR_PX4    = 0x26AC;
+    public static final int DEVICE_PX4FMU = 0x0011;
+
+    public static final int VENDOR_ARDUPILOT_FUTURE = 0x16D0;
+    public static final int DEVICE_ARDUPILOT_FUTURE = 0x0E65;
 
 
-    public static final int VENDOR_CUBE_BOARD        = 0x16D0;
-    public static final int VENDOR_CUBE_BOARD2       = 0x2DAE;
-    public static final int DEVICE_CubeBlack_PLUS    = 0x1101;
-    public static final int DEVICE_CubeBlack         = 0x1011;
-    public static final int DEVICE_CubeBlack_BOOT    = 0x1001;
-    public static final int DEVICE_CubeOrange        = 0x1016;
-    public static final int DEVICE_CubePurple        = 0x1015;
-    public static final int DEVICE_CubePurple_BOOT   = 0x1005;
-    public static final int DEVICE_CubeYellow        = 0x1012;
-    public static final int DEVICE_CubeYellow_BOOT   = 0x1002;
+    public static final int VENDOR_CUBE_BOARD       = 0x16D0;
+    public static final int VENDOR_CUBE_BOARD2      = 0x2DAE;
+    public static final int DEVICE_CubeBlack_PLUS   = 0x1101;
+    public static final int DEVICE_CubeBlack        = 0x1011;
+    public static final int DEVICE_CubeBlack_BOOT   = 0x1001;
+    public static final int DEVICE_CubeOrange       = 0x1016;
+    public static final int DEVICE_CubePurple       = 0x1015;
+    public static final int DEVICE_CubePurple_BOOT  = 0x1005;
+    public static final int DEVICE_CubeYellow       = 0x1012;
+    public static final int DEVICE_CubeYellow_BOOT  = 0x1002;
 
 
+    public static final int VENDOR_VRBrain_BOARD    = 0x27AC;
+    public static final int DEVICE_VRBrain_v51      = 0x1151;
+    public static final int DEVICE_VRBrain_v52      = 0x1152;
+    public static final int DEVICE_VRBrain_v54      = 0x1154;
+    public static final int DEVICE_VRCore_v10       = 0x1910;
+    public static final int DEVICE_VRUBrain_v51     = 0x1351;
 
-    public static final int VENDOR_VRBrain_BOARD     = 0x27AC;
-    public static final int DEVICE_VRBrain_v51       = 0x1151;
-    public static final int DEVICE_VRBrain_v52       = 0x1152;
-    public static final int DEVICE_VRBrain_v54       = 0x1154;
-    public static final int DEVICE_VRCore_v10        = 0x1910;
-    public static final int DEVICE_VRUBrain_v51      = 0x1351;
-
-    public static final int VENDOR_Holybro_BOARD     = 0x3162;
-    public static final int DEVICE_Holybro_Durandal  = 0x004B;
-    public static final int DEVICE_Holybro_Pixhawk5X = 0x0051;
-
-    // at www.linux-usb.org/usb.ids listed for NXP/LPC1768, but all processors supported by ARM mbed DAPLink firmware report these ids
-    public static final int VENDOR_ARM = 0x0d28;
-    public static final int ARM_MBED = 0x0204;
-
+    public static final int VENDOR_Holybro_BOARD    = 0x3612;
+    public static final int DEVICE_Holybro_Durandal = 0x004B;
 
     private UsbId() {
-        throw new IllegalAccessError("Non-instantiable class.");
+        throw new IllegalAccessError("Non-instantiable class");
     }
 
 }
---
+++ ClientLib/src/main/java/org/droidplanner/services/android/impl/communication/connection/usb/UsbHohoConnection.java
@@ -0,0 +1,229 @@
+package org.droidplanner.services.android.impl.communication.connection.usb;
+
+import static com.hoho.android.usbserial.driver.UsbSerialPort.STOPBITS_1;
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+import com.o3dr.services.android.lib.gcs.link.LinkConnectionStatus;
+
+import java.io.IOException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicReference;
+
+import timber.log.Timber;
+
+public class UsbHohoConnection extends UsbConnection.UsbConnectionImpl {
+
+
+    private static final String TAG = UsbHohoConnection.class.getSimpleName();
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    public final static int     USB_READ_WAIT = 500;
+    public final static int     USB_WRITE_WAIT = 500;
+
+    private static final IntentFilter intentFilter = new IntentFilter(ACTION_USB_PERMISSION);
+    private enum UsbPermission { Unknown, Requested, Granted, Denied }
+    private boolean mConnected = false;
+    private UsbPermission usbPermission = UsbPermission.Unknown;
+
+    private final AtomicReference<Bundle> extrasHolder = new AtomicReference<>();
+    private final PendingIntent usbPermissionIntent;
+
+    private UsbSerialPort usbSerialPort;
+    private SerialInputOutputManager usbIoManager;
+
+    private ScheduledExecutorService scheduler;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                removeWatchdog();
+                UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                    if (device != null) {
+                        //call method to set up device communication
+                        try {
+                            UsbSerialDriver driver = UsbSerialProber.getDefaultProber().probeDevice(device);
+                            openUsbDevice(driver, extrasHolder.get());
+                        } catch (IOException e) {
+                            Log.e(TAG, e.getMessage(), e);
+                        }
+                    } else {
+                        LinkConnectionStatus connectionStatus = LinkConnectionStatus
+                            .newFailedConnectionStatus(LinkConnectionStatus.LINK_UNAVAILABLE, "Unable to access usb device.");
+                        onUsbConnectionStatus(connectionStatus);
+                    }
+                } else {
+                    Log.d(TAG, "permission denied for device " + device);
+                    LinkConnectionStatus connectionStatus = LinkConnectionStatus
+                        .newFailedConnectionStatus(LinkConnectionStatus.PERMISSION_DENIED, "USB Permission denied.");
+                    onUsbConnectionStatus(connectionStatus);
+                }
+            }
+      }
+  };
+
+    protected UsbHohoConnection(Context context, UsbConnection parentConn, int baudRate) {
+        super(context, parentConn, baudRate);
+        this.usbPermissionIntent = PendingIntent.getBroadcast(context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+
+    }
+    private void registerUsbPermissionBroadcastReceiver() {
+        mContext.registerReceiver(broadcastReceiver, intentFilter);
+    }
+
+    private void unregisterUsbPermissionBroadcastReceiver() {
+        try {
+            mContext.unregisterReceiver(broadcastReceiver);
+        }catch(IllegalArgumentException e){
+            Timber.e(e, "Receiver was not registered.");
+        }
+    }
+
+    private void removeWatchdog() {
+        if (scheduler != null) {
+            scheduler.shutdown();
+            scheduler = null;
+        }
+    }
+    private final Runnable permissionWatchdog = new Runnable() {
+        @Override
+        public void run() {
+            Log.d(TAG, "Permission request timeout.");
+            LinkConnectionStatus connectionStatus = LinkConnectionStatus
+                    .newFailedConnectionStatus(LinkConnectionStatus.TIMEOUT, "Unable to get usb access.");
+            onUsbConnectionStatus(connectionStatus);
+
+            removeWatchdog();
+        }
+    };
+    @Override
+    protected void closeUsbConnection() throws IOException {
+        mConnected = false;
+        if(usbIoManager != null) {
+            usbIoManager.setListener(null);
+            usbIoManager.stop();
+        }
+        usbIoManager = null;
+        try {
+            usbSerialPort.close();
+        } catch (IOException ignored) {}
+        usbSerialPort = null;
+    }
+
+    @Override
+    protected void openUsbConnection(Bundle extras) throws IOException {
+        extrasHolder.set(extras);
+        registerUsbPermissionBroadcastReceiver();
+
+        UsbDevice device = null;
+        UsbManager usbManager = (UsbManager) mContext.getSystemService(Context.USB_SERVICE);
+        for (UsbDevice v : usbManager.getDeviceList().values())
+//            if(v.getDeviceId() == deviceId)
+//                device = v;
+            device = v;
+        if (device == null) {
+            Log.d(TAG, "connection failed: device not found");
+            return;
+        }
+        UsbSerialDriver driver = UsbSerialProber.getDefaultProber().probeDevice(device);
+        if (driver == null) {
+            driver = CustomProber.getCustomProber().probeDevice(device);
+        }
+        if (driver == null) {
+            Log.d(TAG, "connection failed: no driver for device");
+            return;
+        }
+        if (driver.getPorts().size() < 1) {
+            Log.d(TAG, "connection failed: not enough ports at device");
+            return;
+        }
+
+        openUsbDevice(driver, extras);
+    }
+
+    protected void openUsbDevice (UsbSerialDriver driver, Bundle extras) throws IOException {
+        UsbManager usbManager = (UsbManager) mContext.getSystemService(Context.USB_SERVICE);
+
+        usbSerialPort = driver.getPorts().get(0);
+
+        UsbDeviceConnection usbConnection = usbManager.openDevice(driver.getDevice());
+        if(usbConnection == null && usbPermission == UsbPermission.Unknown && !usbManager.hasPermission(driver.getDevice())) {
+            usbPermission = UsbPermission.Requested;
+            PendingIntent usbPermissionIntent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), 0);
+            usbManager.requestPermission(driver.getDevice(), usbPermissionIntent);
+            return;
+        }
+        if(usbConnection == null) {
+            if (!usbManager.hasPermission(driver.getDevice()))
+                Log.d(TAG,"connection failed: permission denied");
+            else
+                Log.d(TAG,"connection failed: open failed");
+            return;
+        }
+
+        try {
+            usbSerialPort.open(usbConnection);
+            usbSerialPort.setParameters(mBaudRate, UsbSerialPort.DATABITS_8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+//            if(withIoManager) {
+//                usbIoManager = new SerialInputOutputManager(usbSerialPort, this);
+//                usbIoManager.start();
+//            }
+            Log.d(TAG,"connected");
+            mConnected = true;
+            onUsbConnectionOpened(extras);
+
+        } catch (Exception e) {
+            Log.d(TAG,"connection failed: " + e.getMessage());
+            closeUsbConnection();
+        }
+    }
+
+    @Override
+    protected int readDataBlock(byte[] readData) throws IOException {
+        // Read data from driver. This call will return up to readData.length bytes.
+        // If no data is received it will timeout after 200ms (as set by parameter 2)
+        if(usbSerialPort == null)
+            throw new IOException("Device is unavailable.");
+
+        int iavailable = 0;
+        try {
+            iavailable = usbSerialPort.read(readData, USB_READ_WAIT);
+        } catch (NullPointerException e) {
+            final String errorMsg = "Error Reading: " + e.getMessage()
+                    + "\nAssuming inaccessible USB device.  Closing connection.";
+            Log.e(TAG, errorMsg, e);
+            throw new IOException(errorMsg, e);
+        }
+
+        if (iavailable == 0)
+            iavailable = -1;
+        return iavailable;
+    }
+
+    @Override
+    protected void sendBuffer(byte[] buffer) {
+        if (usbSerialPort != null) {
+            try {
+                usbSerialPort.write(buffer, USB_WRITE_WAIT);
+            } catch(IOException e){
+                Log.e(TAG, "Error Sending: " + e.getMessage(), e);
+            }
+        }
+    }
+}
