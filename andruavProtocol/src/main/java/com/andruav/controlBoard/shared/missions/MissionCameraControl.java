9a5b4ddc531c97a1331d86d49718a3d2a36effea
mhefny <mohammad.hefnY@gmail.com>
2022-08-06 16:07:04 +0200

    fix: andruav mission item id to match Ardupilot.

--- andruavProtocol/src/main/java/com/andruav/controlBoard/shared/missions/MissionCameraControl.java
+++ andruavProtocol/src/main/java/com/andruav/controlBoard/shared/missions/MissionCameraControl.java
@@ -2,7 +2,7 @@ package com.andruav.controlBoard.shared.missions;
 
 public class MissionCameraControl extends MissionBase {
 
-    public final static int TYPE_CAMERA_CONTROL = 206; // same as ardupilot
+    public final static int TYPE_CAMERA_CONTROL = 203; // same as ardupilot
 
 
     public MissionCameraControl()
--- andruavProtocol/src/main/java/com/andruav/controlBoard/shared/missions/MissionCameraTrigger.java
+++ andruavProtocol/src/main/java/com/andruav/controlBoard/shared/missions/MissionCameraTrigger.java
@@ -2,7 +2,7 @@ package com.andruav.controlBoard.shared.missions;
 
 public class MissionCameraTrigger extends MissionBase {
 
-    public final static int TYPE_CAMERA_TRIGGER = 203; // same as ardupilot
+    public final static int TYPE_CAMERA_TRIGGER = 206; // same as ardupilot
 
 
     public MissionCameraTrigger()
