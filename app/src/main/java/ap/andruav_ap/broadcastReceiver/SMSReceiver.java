baf255138f796de63f5a7ac52bc0aa03bd843839
mhefny <mohammad.hefnY@gmail.com>
2024-05-05 02:31:54 +0300

    Fix: SMS permissions

--- app/src/main/java/ap/andruav_ap/broadcastReceiver/SMSReceiver.java
+++ app/src/main/java/ap/andruav_ap/broadcastReceiver/SMSReceiver.java
@@ -5,44 +5,31 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
+import android.util.Log;
 
-public class SMSReceiver  extends BroadcastReceiver {
-    private static final String SMS_RECEIVED_ACTION = "android.provider.Telephony.SMS_RECEIVED";
+public class SMSReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "SMSReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction() != null && intent.getAction().equals(SMS_RECEIVED_ACTION)) {
-            // Retrieve the SMS message
-            SmsMessage[] messages = getMessagesFromIntent(intent);
-            if (messages != null && messages.length > 0) {
-                StringBuilder sb = new StringBuilder();
-                for (SmsMessage smsMessage : messages) {
-                    String sender = smsMessage.getDisplayOriginatingAddress();
-                    String messageBody = smsMessage.getMessageBody();
-                    sb.append("SMS from: ").append(sender).append("\n");
-                    sb.append("Message: ").append(messageBody).append("\n\n");
-                }
+        if (intent.getAction() != null && intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")) {
+            Bundle extras = intent.getExtras();
+            if (extras != null) {
+                Object[] pdus = (Object[]) extras.get("pdus");
+                if (pdus != null) {
+                    for (Object pdu : pdus) {
+                        SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdu);
+                        String sender = smsMessage.getDisplayOriginatingAddress();
+                        String messageBody = smsMessage.getMessageBody();
 
-                // Do something with the received SMS
-                String smsContent = sb.toString();
-                // Here, you can process or display the received SMS as per your app's requirements
-            }
-        }
-    }
+                        Log.d(TAG, "Received SMS from: " + sender);
+                        Log.d(TAG, "Message: " + messageBody);
 
-    private SmsMessage[] getMessagesFromIntent(Intent intent) {
-        SmsMessage[] messages = null;
-        try {
-            Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
-            if (pdus != null) {
-                messages = new SmsMessage[pdus.length];
-                for (int i = 0; i < pdus.length; i++) {
-                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                        // Process or display the received SMS as per your app's requirements
+                    }
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return messages;
     }
 }
